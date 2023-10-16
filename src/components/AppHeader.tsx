b40a3977268127b361d7692b94f6cc58e1b49581
Mateusz <108770502+musior@users.noreply.github.com>
2023-10-16 10:57:08 +0200

    Create header component
    Add logo and button images.
    Create a header component with display logo, cards amount and button to add new card.

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./AppHeader.module.css";
+
+export const AppHeader = ({cardsAmount = 0 as number}) => (
+  <header className={styles.header}>
+    <div className={styles.headerBarLeft}>
+      <img
+        src="../../public/logoImage.svg"
+        alt="logo Fischkapp"
+        className={styles.headerBarLogo}
+      />
+      <p>
+        Cards: <span>{cardsAmount}</span>
+      </p>
+    </div>
+    <div className={styles.headerBarRight}>
+      <button>
+        <img src="../../public/ctaButton.svg" alt="cta button" />
+      </button>
+    </div>
+  </header>
+);
